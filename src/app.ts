b0ab8c57ad532441b6d4b49dda45ecc59a430ada
RafaelSSantiago <raphasantiago768@gmail.com>
2024-09-20 00:04:00 -0300

    feat: add controller

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { UserRepositoryImpl } from "./infrastructure/repositories/user.repository.impl";
+import { UserService } from "./application/services/User.service";
+import { UserController } from "./interfaces/controllers/User.controller";
+
+const app =  express();
+app.use(express.json);
+
+
+// instanciando camadas
+const userRepository = new UserRepositoryImpl();
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+
+app.post('/users', (req, res) => userController.createUser(req, res))
+app.get('/users/:id', (req, res) => userController.getUserById(req, res))
